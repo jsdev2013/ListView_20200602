bcbf9732d32488b9c26a17f56e85fa062cde5b51
JISU LEE <jsnote2013@gmail.com>
2020-06-01 21:08:01 +0900

    한줄 표시한 XML 추가

--- app/src/main/java/com/jisu/listview_20200601/datas/Student.kt
+++ app/src/main/java/com/jisu/listview_20200601/datas/Student.kt
@@ -1,6 +1,5 @@
 package com.jisu.listview_20200601.datas
 
-class Student(val name: String, val birthYeat: Int, val isMale:Boolean) {
-
+class Student(val name: String, val birthYear: Int, val isMale:Boolean) {
 //    val name:String = ""
 }

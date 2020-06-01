61782fd112696c4b4781aac2525f103c206f4a14
JISU LEE <jsnote2013@gmail.com>
2020-06-01 20:45:57 +0900

    BaseActivity 정의

--- app/src/main/java/com/jisu/listview_20200601/BaseActivity.kt
+++ app/src/main/java/com/jisu/listview_20200601/BaseActivity.kt
@@ -3,5 +3,8 @@ package com.jisu.listview_20200601
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
-    
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
 }

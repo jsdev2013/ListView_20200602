6353b73669b535016f531fb694139a89c3205cde
JISU LEE <jsnote2013@gmail.com>
2020-06-01 21:54:46 +0900

    어댑터를 실제로 만들고 리스트 뷰와 연결

--- app/src/main/java/com/jisu/listview_20200601/MainActivity.kt
+++ app/src/main/java/com/jisu/listview_20200601/MainActivity.kt
@@ -2,8 +2,15 @@ package com.jisu.listview_20200601
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.jisu.listview_20200601.adapters.StudentAdapter
+import com.jisu.listview_20200601.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    val students = ArrayList<Student>()
+    lateinit var studentAdapter: StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +24,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        students.add(Student("일일일", 1954, true))
+        students.add(Student("이이이", 1984, false))
+        students.add(Student("삼삼삼", 1987, false))
+        students.add(Student("시시시", 1986, false))
+        students.add(Student("오오오", 1984, true))
 
+        studentAdapter = StudentAdapter(mContext, R.layout.student_list_item, students)
+        studentListView.adapter = studentAdapter
     }
-
-
 }
--- app/src/main/java/com/jisu/listview_20200601/adapters/StudentAdapter.kt
+++ app/src/main/java/com/jisu/listview_20200601/adapters/StudentAdapter.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.drawerlayout.R
+import androidx.customview.R
 import com.jisu.listview_20200601.datas.Student
 
 class StudentAdapter(context: Context, resId: Int, list: List<Student>) :ArrayAdapter<Student>(context, resId, list) {

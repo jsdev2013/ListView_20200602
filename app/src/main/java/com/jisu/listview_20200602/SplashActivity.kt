f14b358ff7cbfd6751ec2210fc4bd27eae28fc1c
JISU LEE <jsnote2013@gmail.com>
2020-06-03 22:08:23 +0900

    세팅 조정

---
+++ app/src/main/java/com/jisu/listview_20200602/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.jisu.listview_20200602
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}
